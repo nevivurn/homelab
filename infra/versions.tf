1378b2ad817763b743f57f4bad58f8b7291c685d
Yongun Seong <nevivurn@nevi.dev>
2026-01-24 16:31:35 +0900

    feat(infra): use gcs backend

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "gcs" {
+    bucket                      = "nevi-dev-root-tfstate"
+    prefix                      = "homelab"
+    impersonate_service_account = "root-tf@nevi-dev-root.iam.gserviceaccount.com"
+  }
   required_providers {
     infra = {
       source = "nevivurn/infra"
